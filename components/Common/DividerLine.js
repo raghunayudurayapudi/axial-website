3b9731d969c5922b752ae45d0dafb79fb85c16fb
Rayudu Rayapudi <rayudu.rayapudi@gmail.com>
2023-02-26 20:02:31 -0600

    new-changes-suprt

--- components/Common/DividerLine.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function DividerLine({ props }) {
-  return <hr className="h-[3px] border-none bg-tulip" {...props} />;
-}
--- utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const UPLOADS_URL = "https://d19o6pfnvd4rhh.cloudfront.net/wm-facelift";
-export const SERVICEABLE_PINCODES_COUNT = 29000;
-export const INVISIBLE_CHARACTER = "‎";
